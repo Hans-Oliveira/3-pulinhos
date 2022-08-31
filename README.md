309efb953814b49d396a1f72560ff4fbc4595266
Matheus Hans Duffeck de Oliveira <hansduffeck@gmail.com>
2022-08-31 10:45:48 -0300

    Angular
    Subindo as dependências do angular CLI
    versão 13

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # 3-pulinhos
- Desafio Angular + API 3 Pulinhos
+<h1>Desafio Angular + API 3 Pulinhos</h1>
+<br>
+<p>Foi implementado neste desafio, o consumo de uma API RESTFul e uma manipulação do DOM utilizando Angular v13. Tabém foi utilizado TypeScript e SASS</p>
+<br>
+<br>
+<h4>Segue desafio listado</h4>
+<p>
+O desafio desse projeto é desenvolver uma aplicação em Angular 2 ou qualquer versão
+superior (ex: 12,13…). Consumindo uma API Restful.
+Para desenvolvimento do teste será utilizado uma API para testes já pronta não precisando
+criar ou configurar nenhum servidor ou serviço:
+https://reqres.in/
+Acesse a documentação da API no link abaixo para conhecer os serviços e
+endpoints.
+É desejável que o projeto tenha 2 páginas:
+- Listagem de usuários com paginação
+- Página de detalhe de usuário com atualização e exclusão
+Na listagem de usuários é desejável que se exiba uma lista de usuários realizando
+um get na API reqres.in, deve se exibir Nome, Sobrenome, Foto e Email. Ao clicar
+na pessoa listada deve ser redirecionado para a página de de detalhes.
+Durante a chamada da lista, adicione um delay de 5 segundos para exibir uma
+mensagem de carregando ou uma animação, exemplo de adição do delay:
+https://reqres.in/api/users?delay=3
+Na página de detalhes é desejável que exiba informações para edição do usuário
+(não precisa da foto).
+Entrega
+Ao final do projeto, crie um repositório no github e faça commit da sua aplicação
+com instruções no README.MD para execução do projeto.
+Requisitos:
+- Utilizar Angular 2+ ou versão superior;
+- Uso de TypeScript no Angular;
+- Utilizar SCSS ou SASS para estilização das páginas.
+- Uso do git para controle de versionamento e repositório GitHub
+</p>
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,11 @@ then((res) => {
     return res
 }).
 then((res) => res.json()).
-then((data) => const response = data.data). //data
+then((data) => {
+    const response = data.data
+    console.log(response)
+    return response
+}).
 catch((error) => console.log(error))
 
     /* POST */
@@ -18,6 +22,69 @@ catch((error) => console.log(error))
 fetch('https://reqres.in/api/users', {
     method: "POST",
     headers: {
-        
+        "Content-type" : "application/json"
     },
-})
+    body: JSON.stringify({
+        name: 'teste',
+        job: 'teste'
+    })
+}).
+then((res) => {
+    if (res.ok) {
+        console.log('Requiasição HTTP OK!')
+    }else {
+        console.log('Falha na requisição HTTP!')
+    }
+    return res
+}).
+then((res) => res.json()).
+then((data) => {
+    const response = data
+    console.log(response)
+}).
+catch((error) => console.log(error))
+
+    /* PUT */
+
+fetch('https://reqres.in/api/users/2', {
+    method: "POST",
+    headers: {
+        "Content-type" : "application/json"
+    },
+    body: JSON.stringify({
+        /* name: name,
+        job: job */
+    })
+}).
+then((res) => {
+    if (res.ok) {
+        console.log('Requisição HTTP OK!')
+    }else {
+        console.log('Falha na requisição HTTP!')
+    }
+    return res
+}).
+then((res) => res.json()).
+then((data) => {
+    const response = data
+    console.log(response)
+}).
+catch((error) => console.log(error))
+
+    /* DELETE */
+
+fetch('https://reqres.in/api/users/2', {
+    method: "DELETE",
+    headers: {
+        "Content-type" : "application/json"
+    }
+}).
+then((res) => {
+    if (res.ok) {
+        console.log('Requisição HTTP OK!')
+    }else {
+        console.log('Falha na requisição HTTP!')
+    }
+    return res
+}).
+catch((error) => console.log(error))
